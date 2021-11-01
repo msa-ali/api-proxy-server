62b0a11ef06f2a51a1bcee4dfcf903d1a3a665d9
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2021-11-01 21:07:52 +0530

    proxy server changes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Routes
+app.use('/api', require('./routes'));
+
+// Enable cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const needle = require('needle');
+const url = require('url');
+
+const router = express.Router();
+
+// ENV VARS
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get('/', async (req, res) => {
+    try {
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE,
+            ...url.parse(req.url, true).query
+        });
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+        const data = apiRes.body;
+
+        // Log the request to the Public API
+        if(process.env.NODE_ENV !== 'production') {
+            console.log(`Request: ${API_BASE_URL}?${params}`);
+        }
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({error});
+    }
+});
+
+module.exports = router;
